d62471efa7f1cfd38f5ab1b4af0ef569d5003a0a
santhoshvgts <santhosh@vishgyana.com>
2021-06-24 23:42:48 +0530

    Changes in master

--- lib/vgts_plugin.dart
+++ lib/vgts_plugin.dart
@@ -3,8 +3,6 @@ import 'package:get_it/get_it.dart';
 import 'package:vgts_plugin/form/app_model.dart';
 import 'package:vgts_plugin/form/config/form_field_config.dart';
 
-export 'form/dropdown_field.dart';
-export 'form/edit_text_field.dart';
 export 'form/vgts_form.dart';
 
 GetIt getIt = GetIt.instance;
