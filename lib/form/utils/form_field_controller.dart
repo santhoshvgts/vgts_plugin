70bc484a1c4855706783b6abdfd818f62f990342
santhoshvgts <santhosh@vishgyana.com>
2021-06-25 00:15:29 +0530

    Change

--- lib/form/utils/form_field_controller.dart
+++ lib/form/utils/form_field_controller.dart
@@ -49,7 +49,7 @@ class EmailFormFieldController extends FormFieldController {
   EmailFormFieldController(Key fieldKey,  { bool required = true }) : super(fieldKey, required: required);
 
   @override
-  String? Function(String? p1)? get validator => this.required || this.text.isNotEmpty ? InputValidator.emailValidator : null;
+  String? Function(String? p1)? get validator => this.required ||  this.text.isNotEmpty ? InputValidator.emailValidator : null;
 
   @override
   TextInputType get textInputType => TextInputType.emailAddress;
