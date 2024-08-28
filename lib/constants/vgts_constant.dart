90f1a66d656e04fcbcc5d005aa6b239832ee7aa3
manikandanvgts <manikandan@vishgyana.com>
2024-08-28 15:27:05 +0530

    package version upgraded

--- lib/constants/vgts_constant.dart
+++ lib/constants/vgts_constant.dart
@@ -1,4 +1,8 @@
+import 'dart:io';
+import 'dart:math';
+
 import 'package:intl/intl.dart';
+import 'package:path_provider/path_provider.dart';
 
 sealed class VgtsConstant {
   static NumberFormat currencyFormatter(
@@ -17,5 +21,25 @@ sealed class VgtsConstant {
   static double percentValue(String value) {
     final formatValue = value.replaceAll('%', '').replaceAll('.', '').trim();
     return double.parse(formatValue.isEmpty ? '0' : formatValue);
+  }
+
+  static Future<String> getFileSize(String filepath, [int decimals = 2]) async {
+    var file = File(filepath);
+    int bytes = await file.length();
+    if (bytes <= 0) return "0 B";
+    const suffixes = ["B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
+    var i = (log(bytes) / log(1024)).floor();
+    return ((bytes / pow(1024, i)).toStringAsFixed(decimals)) +
+        ' ' +
+        suffixes[i];
+  }
+
+  static Future<(Directory directory, String fileName)> createTempDirectory(
+      {String? fileName}) async {
+    final tempDir = await getTemporaryDirectory();
+    final _random = Random();
+    final randomFileName =
+        '${fileName != null ? fileName.split('.').first : 'temp'}_${(_random.nextInt(10000) + DateTime.now().millisecondsSinceEpoch).toString()}';
+    return (tempDir, randomFileName);
   }
 }
--- lib/services/image_picker_services.dart
+++ lib/services/image_picker_services.dart
@@ -1,15 +1,17 @@
 import 'dart:io';
 
-import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+
+import 'package:image/image.dart' as img;
 import 'package:image_picker/image_picker.dart';
 import 'package:image_cropper/image_cropper.dart';
-import 'package:path_provider/path_provider.dart';
-import 'package:vgts_plugin/widget/choose_image_widget.dart';
 import 'package:flutter_image_compress/flutter_image_compress.dart';
 
 import 'add_text_watermark.dart';
+import '../widget/choose_image_widget.dart';
+import '../constants/vgts_constant.dart';
 
 class ImagePickerService {
   final _picker = ImagePicker();
@@ -31,9 +33,8 @@ class ImagePickerService {
 
       if (imageSource == null) return [];
 
-      List<XFile?> selectedFile = isMultiPicker
-          ? await _picker.pickMultiImage(imageQuality: 50)
-          : [await _picker.pickImage(source: imageSource, imageQuality: 50)];
+      List<XFile?> selectedFile = await _pickImage(
+          {'isMultiPicker': isMultiPicker, 'imageSource': imageSource});
 
       if (selectedFile.isEmpty ||
           selectedFile.where((n) => n == null).isNotEmpty) {
@@ -42,27 +43,71 @@ class ImagePickerService {
 
       showLoadingIndicator(context);
 
-      List<File> files =
-          isCompressed ? [] : selectedFile.map((e) => File(e!.path)).toList();
+      final imagesList = selectedFile.map((e) => File(e!.path)).toList();
+
+      List<File> waterMarkFiles = [];
+      if (isWaterMater && imagesList.isNotEmpty) {
+        for (final e in imagesList) {
+          final formatter = DateFormat('dd/MM/yyyy h:mm a');
+          final waterMarkFile = await AddTextWaterMark.addTextWaterMark(e,
+              text: waterMarkText ?? '${formatter.format(DateTime.now())}');
+          waterMarkFiles.add(waterMarkFile!);
+        }
+      }
+
+      List<File> files = [];
 
       if (isCompressed) {
-        for (final e in selectedFile) {
-          final file = File(e!.path);
-          final compressed = await _compressImage(file);
+        final images = waterMarkFiles.isNotEmpty ? waterMarkFiles : imagesList;
+        for (final e in images) {
+          final path = e.path;
+          final convertImage =
+              path.contains('.png') ? await convertPngToJpg(path) : e;
+          final compressed = await _compressImage(convertImage);
           if (compressed != null) files.add(File(compressed.path));
         }
       }
 
-      List<File> waterMarkFiles =
-          await addWaterMarks(files: files, isWaterMater: true) ?? files;
-
-      files = waterMarkFiles;
-
       Navigator.pop(context);
+      if (files.isEmpty) return waterMarkFiles;
       return files;
     } catch (e) {
       return null;
     }
+  }
+
+  Future<List<XFile?>> _pickImage(Map params) async {
+    bool isMultiPicker = params['isMultiPicker'];
+    ImageSource? imageSource = params['imageSource'];
+    if (isMultiPicker) return (await _picker.pickMultiImage(imageQuality: 50));
+    return [await _picker.pickImage(source: imageSource!, imageQuality: 50)];
+  }
+
+  Future<File> convertPngToJpg(String path) async {
+    final jpgPath = '${path.replaceAll('.png', '.jpg')}';
+    final image = img.decodeImage(File(path).readAsBytesSync());
+    final jpgImage = img.encodeJpg(image!, quality: 50);
+    final file = await File(jpgPath).writeAsBytes(jpgImage);
+    debugPrint('jpg file path ${file.path}');
+    return file;
+  }
+
+  Future<XFile?> _compressImage(File? image) async {
+    final path = image?.path;
+    final dir = await VgtsConstant.createTempDirectory();
+    final isPng = path?.contains('.png') == true;
+    debugPrint(
+        'image-size before compress : ${path != null ? await VgtsConstant.getFileSize(path) : 0}');
+
+    final file = await FlutterImageCompress.compressAndGetFile(
+        path!, '${dir.$1.absolute.path}/${dir.$2}.jpg',
+        format: (isPng) ? CompressFormat.png : CompressFormat.jpeg,
+        minWidth: 600,
+        minHeight: 500,
+        quality: 70);
+    debugPrint(
+        'image-size after compress :  ${file != null ? await VgtsConstant.getFileSize(file.path) : 0}');
+    return file;
   }
 
   Future<List<File>?> addWaterMarks(
@@ -82,20 +127,8 @@ class ImagePickerService {
     return files;
   }
 
-  Future<XFile?> _compressImage(File? filePath) async {
-    final dir = await getTemporaryDirectory();
-    final path = filePath?.absolute.path;
-    final isPng = path?.contains('.png');
-    return await FlutterImageCompress.compressAndGetFile(
-        path!, '${dir.absolute.path}/temp.${isPng! ? 'png' : 'jpg'}',
-        format: (isPng) ? CompressFormat.png : CompressFormat.jpeg,
-        minWidth: 600,
-        minHeight: 500,
-        quality: 90);
-  }
-
   Future<File?> imageCropper(File? selectedFile) async {
-    final path = selectedFile?.absolute.path;
+    final path = selectedFile?.path;
     final isPng = path?.contains('.png');
     final croppedFile = await ImageCropper().cropImage(
       sourcePath: selectedFile!.path,
@@ -108,10 +141,10 @@ class ImagePickerService {
   }
 
   void showLoadingIndicator(BuildContext context) async {
-    showDialog(
+    await showDialog(
         barrierDismissible: false,
         context: context,
-        builder: (_) {
+        builder: (context) {
           return Dialog(
             backgroundColor: Colors.white,
             child: Padding(
